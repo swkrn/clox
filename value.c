f1074980a8ddd1d2c2b706b16f6d66830f2114f2
me <me@hello.com>
2025-08-30 17:11:47 +0700

    strings

--- value.c
+++ value.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -41,6 +43,9 @@ void printValue(Value value)
     case VAL_NUMBER:
         printf("%g", AS_NUMBER(value));
         break;
+    case VAL_OBJ:
+        printObject(value);
+        break;
     }
 }
 
@@ -59,6 +64,14 @@ bool valuesEqual(Value a, Value b)
 
     case VAL_NUMBER:
         return AS_NUMBER(a) == AS_NUMBER(b);
+
+    case VAL_OBJ:
+    {
+        ObjString *aString = AS_STRING(a);
+        ObjString *bString = AS_STRING(b);
+        return aString->length == bString->length &&
+               memcmp(aString, bString, aString->length) == 0;
+    }
 
     default:
         return false;
